c9906fdf5de0913b40518762c6b6730c6bb33799
Brandon <brandon.david.spicer@gmail.com>
2020-05-11 19:09:56 -0700

    Added generations, scraped rules, updated requirements

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
 # cellular-automata
-A project to build flexible and functional tools to calculate, customize, analyze, and visualize cellular automata.
+A python module for creating and visualizing multi-generational 2-dimensional cellular automata (CA).
 
-`ca2d_mat` is a simple implementation of a general 2D cellular automaton in python.
-It uses numpy for calculation and pygame for visualization.
-
-Documentation coming soon. Here are a few usage examples:
-
-### Run Conway's Game of Life
+### Quickstart: Run Conway's Game of Life
 
 ```python
-from ca2d_mat import CellularAutomaton 
-ca = CellularAutomaton(100, 100)
+from ca2d_mat import CA 
+ca = CA(100, 100)
 ca.randomize()
 ca.evolve()
 ```
@@ -18,58 +13,91 @@ Close the pygame window to quit.
 
 ### Visualize a historical game
 
-Unless `save=False` is passed to `ca.evolve()`, games are stored in `ca.history`.
+Unless `save=False` is passed to `ca.evolve()`, games are stored in `CA.history`.
 You can specify which historical game to use by index (`ca.history[5]`).
 By default, the most recent saved game is used.
 
 ```python
-from ca2d_mat import CellularAutomaton 
-ca = CellularAutomaton(100, 100)
+from ca2d_mat import CA 
+ca = CA(100, 100)
 ca.randomize()
-ca.evolve(limit=100)
+ca.evolve(tick=0.1)
 
 ca.animate()
+
 ```
+Animation controls
+* Use `SPACE` to play/pause
+* When paused, use `j/k` to step forward/back
+* Use `r` to reverse time
+* Use `0` to jump to the beginning, and `1-9` to skip around.
 
-Use `SPACE` to pause/play, and use `k/j` to step forward/back when paused.
+### What are multi-generational cellular automata?
 
+There is a wonderful human named Mirek Wojtovicz who has done a lot of work to catalog and visualize lots of different types of cellular automata. His website is here: http://www.mirekw.com/
+<br><br>
+"Generations" is one of the many familites of CA rules that Mirek describes on his site. It's a simple extension of the familiar "Life" family of CAs which inclues Conway's Game of Life. Rather than transitioning from live to dead, cells enter a dying or refractory state where they are not counted as live cells but still take up space. This leads to highly dynamic evolution, often producing glider-like objects and other complex and aesthetically pleasing patters from shockingly simple rules.
 
-### Inputting a custom neighborhood and rules
+<br><br>
+Rules are encoded in strings with format 'S/B/G', where S = survival, B = birth, G = generations.
+Game of Life is encoded as 23/3/2. G = 2 because there are only two states: alive and dead.
+<br><br>
+I scraped Mirek's website for a table of notable "Generations" rules with names and descriptions. You can view it as a pandas dataframe with `CA.rules_table`.
+<br><br>
 
-The neighborhood `ca.hood` is a set of tuples describing the locations of neighbors relative to a cell at (0,0).
-The rule is encoded as two sets, `ca.live_rule` and `ca.dead_rule`.
-<br />
-`ca.live_rule` contains integers that represent the number of live neighbors that would cause a live cell to continue to live.
-<br />
-`ca.dead_rule` contains integers that represent the number of live neighbors that would cause a dead cell to come to life.
-<br />
-If the number of live neighbors is not in the rule, then the cell dies.
-So, for example, Conway's Game of Life is defined by:
-<br />
-
-`ca.hood = {(0,1), (1,1), (1,0), (0,-1), (-1,-1), (-1,0), (-1,1), (1,-1)}`
-<br />
-`ca.live_rule = {2, 3}`
-<br />
-`ca.dead_rule = {3}`
-
-These are the default values.
-
-You can use `input_hood` to visually input a custom neighborhood.
-
+### Running a specific rule
+You can pass a rule string or a rule name to the CA constructor.
 ```python
-from ca2d_mat import CellularAutomaton
-ca = CellularAutomaton(100, 100)
-ca.input_hood()
+ca = CA(100, 100, rule='345/2/4')
 ```
-Use `w/a/s/d` to move the cursor, and `j/k` to add/remove neighbors.
-Close the pygame window when finished, and the neighborhood will automatically update.
-
+345/2/4 is the rule for "Star Wars", one of the coolest CAs I've ever seen. The following is equivalent:
 ```python
-ca.live_rule = {1, 2, 3, 4, 5}
-ca.dead_rule = {3, 4, 5}
-ca.randomize(ratio=0.2)
-ca.evolve()
+ca = CA(100, 100, rule_name='Star Wars')
 ```
+If both `rule` and `rule_name` are passed, `rule_name` is used.
+### Display methods
+A display method is a method that takes a game (3d numpy array) and an index, and draws a pattern on the pygame display as a function of the game and index. Passing a list of display method names to `display_methods` in the `animate()` function will cause those display methods to be used in that order.
+<br><br>
+Right now, the only ones that work are "generations" and "memory". "generations" draws a different color for each generation. "memory" adds up the past n frames and draws varying shades of red corresponding to the number for each cell.
 
+### Cool Examples
+Here are just a few examples of the incredible animations you can create with a multi-gen 2D CA and color map (a list of rgb tuples where the index corresponds to the state of the cell to which that color is assigned)
 
+# make images for these.
+#### game of life with memory
+```python
+from ca2d import CA
+gol = CA(100, 100)
+gol.randomize()
+gol.evolve(limit=1000)
+gol.animate(display_methods=['memory', 'generations'], colors=[CA.Black, (102, 102, 102)])
+```
+![Game of Life with memory](/images/img1.png)
+#### Brian's Brain with memory
+```python
+from ca2d import CA
+bb = CA(100, 100, rule_name='Brian\'s Brain')
+bb.randomize()
+bb.evolve(limit=500)
+bb.animate(display_methods=['memory', 'generations'])
+```
+![Brian's Brain with memory](/images/img2.png)
+(Can you spot the Sierpinski triangles?)
+#### Prairie on fire with fire colors
+```python
+from ca2d import CA
+pf = CA(100, 100, rule_name='Prairie on fire')
+pf.randomize(option='square')
+pf.evolve(limit=500)
+pf.animate(colors=CA.fire_colors)
+```
+![Prairie on fire with fire colors](/images/img3.png)
+#### Meteor Guns
+```python
+from ca2d import CA
+mg = CA(100, 100, rule_name='Meteor Guns')
+mg.randomize(option='square', size=5)
+mg.evolve(limit=500)
+mg.animate(colors=CA.fire_colors)
+```
+![Meteor Guns with fire colors](/images/img4.png)
--- ca2d_mat.py
+++ ca2d_mat.py
@@ -2,35 +2,120 @@
 """
 
 import time
+import os
 import numpy as np
+import pandas as pd
 import pygame as pg
-import matplotlib.pyplot as plt
 from itertools import product
+try:
+    import cPickle as pickle
+except:
+    import pickle
 
-class CellularAutomaton:
-    def __init__(self, rows, columns):
+# TODO: Make 1D CA.
+
+class CA:
+    # Load files
+    if os.path.exists('history.pickle'):
+        with open('history.pickle', 'rb') as pickle_in:
+            history = pickle.load(pickle_in)
+    if os.path.exists('rules.pickle'):
+        with open('rules.pickle', 'rb') as pickle_in:
+            rules_table = pickle.load(pickle_in)
+
+    # Pygame variables
+    size = 5
+    Black = np.array((0,0,0))
+    White = np.array((255,255,255))
+    Red = np.array((255,0,0))
+    Lime = np.array((0,255,0))
+    Blue = np.array((0,0,255))
+    Yellow = np.array((255,255,0))
+    Cyan = np.array((0,255,255))
+    Magenta	= np.array((255,0,255))
+    Silver = np.array((192,192,192))
+    Gray = np.array((128,128,128))
+    Maroon = np.array((128,0,0))
+    Olive = np.array((128,128,0))
+    Green = np.array((0,128,0))
+    Purple = np.array((128,0,128))
+    Teal = np.array((0,128,128))
+    Navy = np.array((0,0,128))
+
+
+    # color lists
+    default_colors = [Black, White, Red, Lime, Blue, Yellow, Cyan,\
+        Magenta, Silver, Gray, Maroon, Olive, Green, Purple, Teal, Navy]
+    default_colors = default_colors * 4 
+    # https://www.schemecolor.com/fire-color-scheme.php
+    fire_colors = [Black, (250, 192, 0), (255, 117, 0), (252, 100, 0), (215, 53, 2), (182, 34, 3), (128, 17, 0)]
+    fire_colors = fire_colors * 4
+    # https://www.schemecolor.com/pastel-color-tones.php
+    pastel_colors = [Black, (255, 223, 211), (254, 200, 216), (210, 145, 188), (149, 125, 173), (224, 187, 228)]
+    pastel_colors = pastel_colors * 4
+       
+
+    def __init__(self, rows, columns, rule='23/3/2', rule_name=None):
         self.rows = rows
         self.columns = columns
         self.grid = np.zeros((self.rows, self.columns))
         self.next_grid = self.grid.copy()
+        # Default neighborhood is the Moore neighborhood
         self.hood = {(0,1), (1,1), (1,0), (0,-1), (-1,-1), (-1,0), (-1,1), (1,-1)}
-        self.live_rule= {2, 3}          # if cell is alive, what values of live neighbors will make it live next
-        self.dead_rule = {3}         # if cell is dead, what values of live neighbors will make it live next
-        self.history = []
+        # If rule name is provided, override rule
+        if rule_name is not None:
+            rule = CA.rules_table[CA.rules_table.Name == rule_name].Rule.iloc[0]
+        self.rule = rule
+        # Get rule sets from rule string
+        rule = rule.split('/')
+        self.survival_rule = set(map(int, rule[0]))          
+        self.birth_rule = set(map(int, rule[1]))
+        self.generations = int(rule[2])
+ 
+
+    def save_history(self):
+        with open('history.pickle', 'wb') as pickle_out:
+            pickle.dump(CA.history, pickle_out)
         
     def describe(self):
         print(f'Rows: {self.rows}')
         print(f'Columns: {self.columns}')
         print(f'Live cells: {self.grid.sum()}')
-        print(f'Live next: {self.next_grid.sum()}')
         print(f'Hood size: {len(self.hood)}')
-        print(f'Live rule: {self.live_rule}')
-        print(f'Dead rule: {self.dead_rule}')
-        print(f'Saved: {len(self.history)}')
+        print(f'Neighborhood: {self.hood}')
+        print(f'Live rule: {self.survival_rule}')
+        print(f'Dead rule: {self.birth_rule}')
+        print(f'Saved: {len(CA.history)}')
 
-    def randomize(self, ratio=0.5):
+    def randomize(self, ratio=0.5, option=None, size = 3):
         rng = np.random.default_rng()
-        self.grid = rng.random([self.rows, self.columns]) > ratio
+        random_grid = rng.random([self.rows, self.columns])
+
+        if option == 'square':
+            # Define coordinates
+            # square is 1/5 rows by 1/5 columns and centered in the display
+            x = self.rows
+            y = self.columns
+            x1 = (x // 2) - (x // (2 * size))
+            x2 = (x // 2) + (x // (2 * size))
+            y1 = (y // 2) - (y // (2 * size))
+            y2 = (y // 2) + (y // (2 * size))
+            
+            self.grid = np.zeros([x, y])
+            self.grid[x1:x2, y1:y2] = random_grid[x1:x2, y1:y2] < ratio
+        else:
+            self.grid = random_grid < ratio
+        self.next_grid = self.grid
+
+    def square_seed(self, size = 3, offset = (0, 0)):
+        x = self.rows 
+        y = self.columns
+        x1 = ((x // 2) - (x // (2 * size)) + offset[0]) % x
+        x2 = ((x // 2) + (x // (2 * size)) + offset[0]) % x
+        y1 = ((y // 2) - (y // (2 * size)) + offset[1]) % y
+        y2 = ((y // 2) + (y // (2 * size)) + offset[1]) % y
+
+        self.grid[x1:x2, y1:y2] = 1
 
     def kill_all(self):
         self.grid = np.zeros([self.rows, self.columns])
@@ -87,7 +172,7 @@ class CellularAutomaton:
         steps = 0
         pg.init()
         screen = pg.display.set_mode((size * self.rows, size * self.columns))
-        pg.display.set_caption('Cellular Automaton 2D (Matrix version)')
+        pg.display.set_caption(f'CA2D rule: {self.rule}')
         
         # pygame loop
         running = True
@@ -111,72 +196,155 @@ class CellularAutomaton:
             self.calc_next()
 
         if save:
-            self.history.append(np.array(history))
-            return np.array(history)
+            CA.history.append(np.array(history))
+            self.save_history()
 
     # vectorized function that implements a single time step of the CA
     def calc_next(self):
+        # create only_live_grid
+        live_grid = self.grid == 1
+        # use that to make ngrid
         # ngrid contains the number of live neighbors for each cell
         ngrid = 0
         for n, m in self.hood:
-            ngrid += np.roll(self.grid, (n, m), axis=(0,1))
+            ngrid += np.roll(live_grid, (n, m), axis=(0,1))
 
         # calculate next_grid from grid and ngrid
-        live_mask = np.isin(ngrid, list(self.live_rule))
-        dead_mask = np.isin(ngrid, list(self.dead_rule))
-        self.next_grid = np.where(self.grid, live_mask, dead_mask)
+        survival_mask = np.logical_and(self.grid == 1, np.isin(ngrid, list(self.survival_rule), invert=True))
+        birth_mask = np.logical_and(self.grid == 0, np.isin(ngrid, list(self.birth_rule)))
 
+        self.next_grid = np.where(survival_mask, (self.grid + 1) % self.generations, self.next_grid)
+        self.next_grid = np.where(birth_mask, 1, self.next_grid)
+        self.next_grid = np.where(self.grid >= 2, (self.grid + 1) % self.generations, self.next_grid)
+                
         self.grid = self.next_grid.copy()
 
-    def animate(self, game = None, tick = 0.1):
-        if game == None:
-            game = self.history[-1]
+    def animate(self, game=None, tick=0, display_methods=['generations'], colors=None, loop=True):
+        if np.all(game == None):
+            game = CA.history[-1]
+        if colors == None:
+            colors = CA.default_colors
         size = 5
-        black, red, green, blue = (0,0,0), (255,0,0), (0,255,0), (0,0,255)
+        black, white, red, green, blue = (0,0,0), (255,255,255), (255,0,0), (0,255,0), (0,0,255)
         pg.init()
         screen = pg.display.set_mode((size * self.rows, size * self.columns))
         index = 0
+        r = 1       # reversal constant
 
         # pygame loop
         running = True
-        paused = False
+        paused = True 
         while running:
-            pg.display.set_caption(f'Cellular Automata 2D (Matrix version) | index = {index}')
+            # controls I want
+                # skip with 0-9
+                # fast forward with h/l
+                # reverse with r
+            pg.display.set_caption(f'CA2D Animation | r = {r != 1} | index = {index}')
             # check events
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
                 if event.type == pg.KEYDOWN:
+
                     # Spacebar to puase/play
                     if event.key == pg.K_SPACE:
                         paused = not paused
-                    # If paused, use k/j to step forward/back
-                    if paused and event.key == pg.K_k:
-                        index += 1
-                        self.display_index(screen, game, index, black, red)
+                    # If paused, use j/k to step forward/back
                     if paused and event.key == pg.K_j:
+                        index += 1
+                    if paused and event.key == pg.K_k:
                         index -= 1
-                        self.display_index(screen, game, index, black, red)
+
+                    if event.key == pg.K_r:
+                        r = -r 
+                    
+                    keys = [pg.K_0, pg.K_1, pg.K_2, pg.K_3, pg.K_4, pg.K_5, pg.K_6, pg.K_7, pg.K_8, pg.K_9]
+                    for k in keys:
+                        if event.key == k:
+                            index = (k - 48) * len(game) // 10
+
             if not paused:
-                # display the grid for given index
-                self.display_index(screen, game, index, black, red)
                 # time delay
                 time.sleep(tick)
-
                 # check if index out of range
-                index += 1
+                index += 1*r
                 if index >= len(game):
+                    index = 0
+
+                if not loop:
                     running = False
 
-    # Display game at given index
-    def display_index(self, screen, game, index, dead_color, live_color):
-        size = 5
-        screen.fill(dead_color)
-        for i, j in product(range(game[index].shape[0]), range(game[index].shape[1])):
-            if game[index][i][j]:
-                pg.draw.rect(screen, live_color, (i*size, j*size, size, size))
-        pg.display.update()
+
+            screen.fill(CA.Black)
+            for name in display_methods:
+                
+                if name == 'memory':
+                    self.display_memory(screen, game, index % len(game), CA.Red, 20)
+                elif name == 'decay':
+                    self.display_decay(screen, game, index % len(game), color_list=colors)
+                elif name == 'generations':
+                    self.display_generations(screen, game, index % len(game), color_list=colors) 
+
+            pg.display.update()
+
+    """================================ Different display functions for animate() ================================"""
+
+    # TODO: write simple display_default function
+    def display_default(self, screen, game, index, color=None):
+        pass
         
+    # Display game at given index
+    def display_generations(self, screen, game, index, color_list):
+        for i, j in product(range(game.shape[1]), range(game.shape[2])):
+            gen = int(game[index][i][j])
+            if game[index][i][j] != 0:
+                pg.draw.rect(screen, color_list[gen], CA.size * np.array([i, j, 1, 1], np.int32))
+
+    def display_memory(self, screen, game, index, live_color, depth):
+        # define memory_grid
+        memory_grid = np.zeros([self.rows, self.columns])
+        # look back min(index, depth) steps in game from current index
+        for d in range(min(index, depth)):
+            # add up the past min(index, depth) number of grids
+            memory_grid += game[index - d]
+        for i, j in product(range(game.shape[1]), range(game.shape[2])):
+            if game[index][i][j]:
+                pg.draw.rect(screen, live_color, (i*CA.size, j*CA.size, CA.size, CA.size))
+            else:
+                mem_color = (memory_grid[i][j]*255)//min(index+1, depth)
+                mem_color = (mem_color % 255, 0, 0)
+                pg.draw.rect(screen, mem_color, (i*CA.size, j*CA.size, CA.size, CA.size))
+
+    def display_colors(self, screen, game, index, color_list=[(0,0,0), (0,255,0), (255,0,0), (0,0,255), (255,255,255)]):
+        # add up the last 3 grids
+        # color based on number
+        memory_grid = np.zeros([self.rows, self.columns])
+        length = len(color_list)
+        
+        for d in range(min(index, length-1)):
+            memory_grid += game[index - d]
+            
+        for i, j in product(range(game.shape[1]), range(game.shape[2])):
+            num = int(memory_grid[i][j])
+            pg.draw.rect(screen, color_list[num], (i*CA.size, j*CA.size, CA.size, CA.size))
+
+    def display_decay(self, screen, game, index, color_list=[(0,0,0), (0,0,255), (0,255,0), (255,0,0), (255,255,255)]):
+        depth = min(index, len(color_list))
+
+        for i, j in product(range(game.shape[1]), range(game.shape[2])):
+            dist = 0
+            for d in range(depth-1):
+                if game[index - d][i][j]:
+                    break
+                else:
+                    dist += 1
+
+            pg.draw.rect(screen, color_list[dist], (i*CA.size, j*CA.size, CA.size, CA.size))
+
+                
+    """ =========================================================================================="""                
+
+    """ Display function for evolve() """        
     # displays the current grid to a given pygame screen
     def display(self, screen, size=5):
         screen.fill((0,0,0))
@@ -188,6 +356,7 @@ class CellularAutomaton:
 
 
 if __name__ == '__main__':
-    game = CellularAutomaton(100, 100)
+    game = CA(100, 100)
     game.randomize()
-    game.evolve(size = 5) 
+    game.evolve(limit=500, viz=False) 
+    game.animate()
